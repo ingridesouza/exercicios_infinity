f20229ed9bb88d2da240587cbeed2b3cfa3a9870
Ingride Souza <134303148+ingridesouza@users.noreply.github.com>
2023-11-30 09:17:26 -0300

    create

---
+++ Python/condicionais.py
@@ -0,0 +1,33 @@
+#Crie uma solicitação de dados ao usuário.
+# emailUsuario = input("Digite o seu email: ")
+# emailEsperado = "ingridesouza040@gmail.com"
+#Imprima a solicitação anterior na tela com uma mensagem personalizada.
+# if emailUsuario == emailEsperado :
+#     print("Email Válido")
+ 
+# else:
+#     print("Email Incorreto")
+
+# senhaUsuario = input("Digite a sua senha: ")
+# senhaEsperada = "senhasenhasenha"
+
+# if senhaUsuario == senhaEsperada:
+#     print("Usuário Logado")
+
+# else:
+#     print("Senha Incorreta")
+
+#---------------------------------------------------------------------
+#Crie um exemplo de cada uma das principais tipagens de variáveis.
+
+# variavel1 = True
+# print(type(variavel1))
+
+# variavel2 = "Palavra"
+# print(type(variavel2))
+
+# variavel3 = 19
+# print(type(variavel3))
+
+# variavel4 = 3.98
+# print(type(variavel4))
