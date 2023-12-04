9f6f2550745b5a7d524125156b56a2e2a3abdf1d
Ingride Souza <134303148+ingridesouza@users.noreply.github.com>
2023-12-04 15:33:56 -0300

    add code

---
+++ logicaProgramacao/loop.py
@@ -0,0 +1,24 @@
+#Loops -> percorrer os valores, adicionar somas de números, repetir funções e muitas outras coisas.
+#Em Python: o FOR e o WHILE.
+
+#FOR(para) - o loop "for" podemos executar um conjunto de instruções, uma vez para cada item de uma lista, tupla, conjunto etc.
+#FOR- repete o comando "print(variavel)"
+
+# coracao = 'amor'
+# for variavel in coracao:
+#     print(variavel)
+
+# Declaramos a variável para guardar a palavra, porém tbm é possivel não declarar variável e colocar diretamente no código a string.
+#Ex.:
+caderno = 'pagina'
+for linha in caderno:
+    print(linha)
+    
+# A variável linha irá iteragir com 'pagina'
+#------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------
+# #WHILE(enquanto) - semelhante ao IF, a diferença semântica entre eles carrega um detalhe bem específico:
+# Na instrução if, quando a condição é atendida, se executa suas instruções apenas uma vez.
+# Na instrução while, enquanto a condição é avaliada como True, as instruções são repetidas.
+
+# O while segue uma lógica bem simples: enquanto há algo para fazer -> faça isso
