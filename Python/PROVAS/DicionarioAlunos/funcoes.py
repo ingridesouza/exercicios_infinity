3cfd352f398037bec062b9214e662f68a29e54e6
Ingride Souza <134303148+ingridesouza@users.noreply.github.com>
2024-01-14 23:11:43 -0300

    update

--- Python/PROVAS/DicionarioAlunos/funcoes.py
+++ Python/PROVAS/DicionarioAlunos/funcoes.py
@@ -1,37 +1,61 @@
-
-
+# Definição da função para adicionar um aluno ao dicionário 'alunos'
 def adicionar_aluno(alunos, nome_aluno, numero_matricula):
+    # Cria um novo dicionário representando o aluno a ser adicionado
     novo_aluno = {
-        'nome_aluno' : nome_aluno,
-        'numero_matricula' : numero_matricula
+        'nome_aluno': nome_aluno,
+        'numero_matricula': numero_matricula
     }
+    # Adiciona o novo aluno a lista de alunos
     alunos.append(novo_aluno)
+    # Imprime uma mensagem indicando que o aluno foi cadastrado com sucesso
     print(f'Aluno {nome_aluno} cadastrado com sucesso!')
 
 
-
+#-----------------------------------------------------------------
+    
+# Definição da função para remover um aluno do dicionário 'alunos'
 def removerAluno(alunos, matricula_usuario_remover):
-    for matricula in alunos:
-        if matricula['numero_matricula'] == matricula_usuario_remover:
-            matricula.remove(matricula)
+    # Percorre a lista de alunos para encontrar o aluno com a matrícula a ser removida
+    for aluno in alunos:
+        if aluno['numero_matricula'] == matricula_usuario_remover:
+            # Remove o aluno da lista
+            alunos.remove(aluno)
+            # Imprime uma mensagem indicando que o aluno foi removido com sucesso
             print(f'Aluno {matricula_usuario_remover} removido com sucesso!')
             return
-        print( f'Matrícula não encontrada')
+    # Se a matrícula não for encontrada, imprime uma mensagem indicando que a matrícula não foi encontrada
+    print('Matrícula não encontrada')
 
 
+#-----------------------------------------------------------------
+
+# Definição da função para atualizar o nome de um aluno no dicionário 'alunos'
 def atualizarAluno(alunos, numero_matricula, nome_aluno_atualizar):
+    # Percorre a lista de alunos para encontrar o aluno com a matrícula a ser atualizada
     for aluno in alunos:
         if aluno['numero_matricula'] == numero_matricula:
-            aluno['nome'] = nome_aluno_atualizar
+            # Atualiza o nome do aluno
+            aluno['nome_aluno'] = nome_aluno_atualizar
+            # Imprime uma mensagem indicando que o nome do aluno foi atualizado com sucesso
             print(f'Nome do aluno com a matrícula {numero_matricula} atualizado com sucesso!')
             return
+    # Se a matrícula não for encontrada, imprime uma mensagem indicando que a matrícula não foi encontrada
     print('Matrícula não encontrada...')
 
 
-def verAlunos(alunos):
-    print("Lista de todos os alunos:")
-    for aluno in alunos:
-        print(f"Matrícula: {aluno['numero_matricula']}, Nome: {aluno['nome']}")
+#-----------------------------------------------------------------
 
+# Definição da função para listar todos os alunos no dicionário 'alunos'
+def verAlunos(alunos):
+    # Imprime uma mensagem indicando que a lista de alunos será exibida
+    print("Lista de todos os alunos:")
+    # Percorre a lista de alunos e imprime as informações de cada aluno
+    for aluno in alunos:
+        print(f"Matrícula: {aluno['numero_matricula']}, Nome: {aluno['nome_aluno']}")
+
+
+#-----------------------------------------------------------------
+
+# Definição da função para imprimir uma linha de caracteres '-' para melhorar a apresentação visual
 def linha():
     print('-'*30)
--- Python/PROVAS/DicionarioAlunos/menu.py
+++ Python/PROVAS/DicionarioAlunos/menu.py
@@ -1,54 +1,70 @@
-# [PY-A07] Você foi contratado(a) para desenvolver um programa que gerencie um dicionário de alunos onde a chave deste dicionário é o número de matrícula dos próprios alunos. O programa deve permitir adicionar, remover, atualizar e listar os alunos. Para isso, você deve implementar um módulo que contém as seguintes funções: 
+# [PY-A07] Você foi contratado(a) para desenvolver um programa que gerencie um dicionário de alunos onde a chave deste dicionário é o número de matrícula dos próprios alunos. 
+# O programa deve permitir adicionar, remover, atualizar e listar os alunos. 
+# Para isso, você deve implementar um módulo que contém as seguintes funções:
+#-----------------------------------------------------------------------------
 
-#AdicionarAluno(): Solicita ao usuário que digite o nome e o número de matrícula de um aluno e adicione-o ao dicionário de alunos. 
 
-#RemoverAluno(): Solicita ao usuário que digite o número de matrícula de um aluno e remove-o do dicionário de alunos. 
-
-#AtualizarAluno(): Solicita ao usuário que digite o número de matrícula de um aluno e atualize o nome desse aluno no dicionário . 
-
-#VerAlunos(): Lista todos os alunos cadastrados, exibindo o número de matrícula e o nome de cada um.
-
+# Importa as funções definidas no módulo 'funcoes' e o módulo 'bancoDados'
 from funcoes import *
 from bancoDados import *
 
+# Loop principal que mantém o programa em execução continuamente
 while True:
-
-        print(''' 
-                1- Adicionar Aluno.
-                2- Remover Aluno.
-                3- Aualizar Aluno.
-                4- Ver Alunos.
-                5- Sair.
-                ''')
     
-        escolha = input('Digite o número da opção desejada: -->')
+    # Imprime o menu de opções para o usuário
+    print(''' 
+            1- Adicionar Aluno.
+            2- Remover Aluno.
+            3- Aualizar Aluno.
+            4- Ver Alunos.
+            5- Sair.
+            ''')
+    
+    # Solicita ao usuário que digite a opção desejada
+    escolha = input('Digite o número da opção desejada: -->')
 
-        match escolha:
-            case '1':
-                nome_aluno = input('Digite o nome do aluno que deseja adicionar: ')
-                numero_matricula = input('Digite o número da matricula do mesmo: ')
-                linha()
-                adicionar_aluno(alunos,nome_aluno, numero_matricula)
-                linha()
+    
+    #Uso a instrução 'match' para avaliar a escolha do usuário e executar a ação correspondente.
+    match escolha:
+        case '1':
+            # Solicita ao usuário o nome e número de matrícula do aluno a ser adicionado
+            nome_aluno = input('Digite o nome do aluno que deseja adicionar: ')
+            numero_matricula = input('Digite o número da matrícula do mesmo: ')
+            linha()
+            # Chama a função 'adicionar_aluno', fornecendo os argumentos inseridos pelo usuário.
+            adicionar_aluno(alunos, nome_aluno, numero_matricula)
+            linha()
 
-            case '2':
-                matricula_usuario_remover = int(input('Digite o número da matrícula do aluno que deseja remover: '))
-                linha()
-                removerAluno(alunos, matricula_usuario_remover)
-                linha()
+        case '2':
+            # Solicita ao usuário o número de matrícula do aluno a ser removido
+            matricula_usuario_remover = int(input('Digite o número da matrícula do aluno que deseja remover: '))
+            linha()
+            # Chama a função 'removerAluno' fornecendo os argumentos inseridos pelo usuário.
+            removerAluno(alunos, matricula_usuario_remover)
+            linha()
 
-            case '3':
-                numero_matricula = input('Digite o número da matricula do aluno que deseja atualizar: ')
-                nome_aluno_atualizar = input('Insira o nome atualizado do aluno: ')
-                linha()
-                atualizarAluno(alunos, numero_matricula, nome_aluno_atualizar)
-                linha()
+        case '3':
+            # Solicita ao usuário o número de matrícula do aluno e o novo nome
+            numero_matricula = input('Digite o número da matrícula do aluno que deseja atualizar: ')
+            nome_aluno_atualizar = input('Insira o nome atualizado do aluno: ')
+            linha()
+            # Chama a função 'atualizarAluno' fornecendo os argumentos inseridos pelo usuário.
+            atualizarAluno(alunos, numero_matricula, nome_aluno_atualizar)
+            linha()
 
-            case '4':
-                linha()
-                verAlunos(alunos)
-                linha()
-            case _:
-                linha()
-                print('ERRO! Tente novamente')
-                linha()
+        case '4':
+            linha()
+            # Chama a função 'verAlunos' para listar todos os alunos cadastrados
+            verAlunos(alunos)
+            linha()
+
+            # Sai do programa usando o break quando a opção '5' é escolhida
+        case '5':
+            print("Saindo do programa. Até logo!")
+            break
+
+        case _:
+            linha()
+            # Imprime uma mensagem de erro se a escolha do usuário não corresponder a nenhuma opção válida
+            print('ERRO! Tente novamente')
+            linha()
