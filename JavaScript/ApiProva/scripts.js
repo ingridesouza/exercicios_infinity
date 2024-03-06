1de7f3fde28c8f45fbf71326cba358f263520e18
Ingride Souza <134303148+ingridesouza@users.noreply.github.com>
2024-03-06 15:31:27 -0300

    upp

---
+++ JavaScript/ApiProva/scripts.js
@@ -0,0 +1,35 @@
+const buttonsContainer = document.getElementById('buttons-container');
+const imagesContainer = document.getElementById('images-container');
+
+// Obtendo raças de cachorros da API
+fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(data => {
+        const breeds = Object.keys(data.message);
+
+        breeds.forEach(breed => {
+            const button = document.createElement('button');
+            button.textContent = breed; // Nome da raça em português
+            button.className = 'button';
+            button.addEventListener('click', () => fetchImages(breed));
+            buttonsContainer.appendChild(button);
+        });
+    })
+    .catch(error => console.error('Erro ao obter raças de cachorros:', error));
+
+// Função para obter e exibir imagens de uma raça específica
+function fetchImages(breed) {
+    imagesContainer.innerHTML = ''; // Limpar imagens anteriores
+
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random/4`)
+        .then(response => response.json())
+        .then(data => {
+            data.message.forEach(imageUrl => {
+                const image = document.createElement('img');
+                image.src = imageUrl;
+                image.className = 'image';
+                imagesContainer.appendChild(image);
+            });
+        })
+        .catch(error => console.error(`Erro ao obter imagens da raça ${breed}:`, error));
+}
