5c2250955d74d5ee4b34a06f197f831e4700aad2
Ingride Souza <134303148+ingridesouza@users.noreply.github.com>
2024-10-02 11:49:52 -0300

    Update database.py

--- Python/ProjetoFinalPY/database.py
+++ Python/ProjetoFinalPY/database.py
@@ -9,7 +9,6 @@ class DatabaseManager:
             password="876@ni2!a",    # Senha do banco de dados
             database="gerenciadorTarefas"  # Nome do banco de dados
         )
-        # Cursor para executar comandos SQL
         self.cursor = self.conexao.cursor()
 
     def criar_tabela_tarefas(self):
@@ -28,27 +27,17 @@ class DatabaseManager:
 
     def salvar_tarefa(self, tarefa):
         query = "INSERT INTO tarefas (nome, descricao, inicio, fim, status) VALUES (%s, %s, %s, %s, %s)"
-        # Valores a serem inseridos na query
         valores = (tarefa.nome, tarefa.descricao, tarefa.inicio, tarefa.fim, tarefa.status)
-        # Executa a query
         self.cursor.execute(query, valores)
-        # Confirma a transação
         self.conexao.commit()
 
     def remover_tarefas_concluidas(self):
-        # Define a query SQL para excluir tarefas concluídas da tabela 'tarefas'
         query = "DELETE FROM tarefas WHERE status = 'Concluído'"
-        # Executa a query
         self.cursor.execute(query)
-        # Confirma a transação
         self.conexao.commit()
 
     def atualizar_status_tarefa(self, nome, novo_status):
-        # Define a query SQL para atualizar o status de uma tarefa na tabela 'tarefas'
         query = "UPDATE tarefas SET status = %s WHERE nome = %s"
-        # Valores a serem inseridos na query
         valores = (novo_status, nome)
-        # Executa a query
         self.cursor.execute(query, valores)
-        # Confirma a transação
         self.conexao.commit()
